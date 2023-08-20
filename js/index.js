05701b1e6f9594348141671af28f318a961e9bd4
PurpleSparkel <142705713+PurpleSparkel@users.noreply.github.com>
2023-08-20 11:52:12 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,35 @@
+// interfaceCode
+var App = {
+    name: '',
+    mounted() {
+
+        // defaultLoad
+        // ShouYeMountedStart
+
+        // ShouYeMountedEnd
+
+    },
+    components: {},
+    data() {
+        return {
+            // insertData
+            // ShouYeDataStart
+
+            // ShouYeDataEnd
+
+        }
+    },
+    methods: {
+        default () {},
+        // insertMethod
+        // ShouYeMethodStart
+
+        // ShouYeMethodEnd
+
+        // methodsCode
+
+    },
+    computed: {}
+}
+const app = Vue.createApp(App);
+app.mount("#defaultId1");
